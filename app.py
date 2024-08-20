c861b76dc53548818c222f7de883a189be7fd63a
V. Prasanna Rajendra <prasanna.rajezzy@gmail.com>
2024-08-20 09:44:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,17 @@
-import requests
-from flask import Flask, render_template, request, jsonify
-
+from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
-
-# Set up your API URL and headers
-API_URL = "https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english"
-headers = {"Authorization": f"Bearer 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"}
-
 
 @app.route('/')
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-    return render_template("index.html")
 
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    <img src="http://loremflickr.com/600/400" />
+    """.format(time=the_time)
 
-
-
-
-# from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
-# import torch
-# # Load the pre-trained model and tokenizer
-# model_name = "distilbert-base-uncased-finetuned-sst-2-english"
-# tokenizer = AutoTokenizer.from_pretrained(model_name)
-# model = AutoModelForSequenceClassification.from_pretrained(model_name)
-
-# # Create a sentiment-analysis pipeline
-# sentiment_analyzer = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
-
-
-# from flask import Flask, render_template, request, jsonify
-# from transformers import pipeline
-
-# app = Flask(__name__)
-
-# # Load the pre-trained model for conversational AI
-# chatbot = sentiment_analyzer
-
-# @app.route("/")
-# def home():
-#     return render_template("index.html")
-
-# @app.route("/get_response", methods=["POST"])
-# def get_response():
-#     user_message = request.json.get("message")
-#     response = chatbot(user_message)
-#     print(response)
-#     response_text = response[0]['label']
-#     response_text= "The given response is :"+response_text
-#     return jsonify({"response": response_text})
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
