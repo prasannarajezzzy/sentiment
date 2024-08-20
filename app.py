f50361f298ea55e127b9f81054357bf343b422d0
V. Prasanna Rajendra <prasanna.rajezzy@gmail.com>
2024-08-20 09:47:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask
+import requests
+from flask import Flask, render_template, request, jsonify
 from datetime import datetime
+
 app = Flask(__name__)
 
+
+# Set up your API URL and headers
+API_URL = "https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english"
+headers = {"Authorization": f"Bearer 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"}
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    print(response.json(),"ssssssssssssss")
+    return response.json()[0]
+    
 @app.route('/')
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
