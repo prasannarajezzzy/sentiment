7a4af99921884e3ef7178293217ffbbffa36acbb
V. Prasanna Rajendra <prasanna.rajezzy@gmail.com>
2024-08-20 09:49:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,14 +17,7 @@ def query(payload):
     
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
